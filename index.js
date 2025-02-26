55edc55c955bc2f0cc87d684bc748d82e034beb6
Muhammad Ali <softeng.aliijaz@gmail.com>
2025-02-27 03:50:15 +0500

    Add initial project setup with Express server and documentation
    - Created package.json for project dependencies and scripts
    - Implemented basic Express server in index.js
    - Added .gitignore to exclude node_modules and environment files
    - Created README.txt with server setup instructions and examples

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Welcome to NodeJs Journey');
+const { log } = require("console");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Server is running");
+});
+
+app.listen(7424, () => {
+    console.log("servve is ");
+});
