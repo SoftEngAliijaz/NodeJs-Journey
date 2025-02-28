bcc36a9391b7096eccba1ef811d31281cb7e0bc8
Muhammad Ali <softeng.aliijaz@gmail.com>
2025-02-28 05:58:00 +0500

    Enhance API functionality by adding new routes for adding data and ping-pong response; refactor route setup for clarity

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (_, res) => {
     res.send("Server is connected and running on http://localhost:1122");
 });
 
+const routes = require("./routes/route");
+app.use("/api/routes", routes);
+
 app.listen(1122, () => {
     console.log("Server is running on http://localhost:1122");
 });
-
-
-const routes = require("./routes/route");
-
-app.use("/routes", routes)
--- routes/route.js
+++ routes/route.js
@@ -1,4 +1,5 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 
 router.get("/getUser", (req, res) => {
     const user = {
@@ -8,6 +9,29 @@ router.get("/getUser", (req, res) => {
     };
 
     res.json(user);
+});
+
+router.post("/addSomething", (req, res) => {
+    console.log("Request Body:", req.body);
+    const request = req.body;
+
+    if (request.username) {
+        res.json(request);
+    } else {
+        res.status(400).json({ "Error": "Not Allowed" });
+    }
+
+});
+
+
+router.patch("/pingpong", (req, res) => {
+    const request = req.body;
+
+    if (request._username === "user") {
+        res.json({ "response": "Success!" });
+    } else {
+        res.status(400).json({ "Error": "Not Allowed" });
+    }
 });
 
 module.exports = router;
