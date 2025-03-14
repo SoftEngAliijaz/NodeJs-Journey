5619c87b0f47e7814de2d524e87c65a13450a91b
Muhammad Ali <softeng.aliijaz@gmail.com>
2025-03-14 17:50:48 +0500

    Update MOCK_DATA.json and enhance API routes with user management features

--- index.js
+++ index.js
@@ -5,26 +5,46 @@ const app = express();
 
 const PORT = 8080;
 
+///show welcome message
+app.get("/", (req, res) => {
+    return res.send("Welcome to NodeJs Journey");
+});
+
+/// get all users list
 app.get("/api/getAllUsers", (req, res) => {
     return res.json(users);
 });
 
-app.route("/api/getAllUsers/:id")
-    .get((req, res) => {
-        const id = Number(req.params.id);
-        const user = users.find((user) => user.id === id);
-        return res.json(user);
-    })
-    .patch((req, res) => {
-        return res.json({ status: "Pending" });
-    })
-    .delete((req, res) => {
-        return res.json({ status: "Pending" });
-    });
-
+/// post new user
 app.post("/api/users", (req, res) => {
     return res.json({ status: "Pending" });
 });
+
+/// specify operations
+app.route("/api/getAllUsers/:id")
+    .get((req, res) => {
+        const id = Number(req.params.id);
+
+        /// check if its a number or not
+        if (isNaN(id)) {
+            res.status(404).json({ status: 404, message: "Invalid ID" });
+        }
+
+        const user = users.find((user) => user.id === id);
+
+        /// now find and show result
+        if (user) {
+            return res.status(200).json(user);
+        } else {
+            return res.status(404).json({ status: 404, message: "User Not Found" });
+        }
+    })
+    .patch((req, res) => {
+        return res.status(501).json({ status: 501, message: "PATCH is Not Implemented Yet" });
+    })
+    .delete((req, res) => {
+        return res.status(501).json({ status: 501, message: "DELETE is Not Implemented Yet" });
+    });
 
 app.listen(PORT, () => {
     console.log("Server is Listening on:" + PORT);
